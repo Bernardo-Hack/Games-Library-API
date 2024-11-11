d135efbddaf4b01185f7f34ae12ded2169f8e645
Bernardo de Lima Hack <102930809+Bernardo-Hack@users.noreply.github.com>
2024-11-11 15:44:47 -0300

    Updated view/url logic
    Now using the APIView instead of api_view

--- GamesLibrary/Games/urls.py
+++ GamesLibrary/Games/urls.py
@@ -1,17 +1,14 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [    
-    path('publishers/', views.list_publishers, name='list_publishers'),
-    path('publisher/<int:id>', views.get_publisher, name='get_publisher'),
-    path('create_publisher/', views.create_publisher, name='create_publisher'),
-    path('update_publisher/<int:id>', views.update_publisher, name='update_publisher'),
-    path('delete_publisher/<int:id>', views.delete_publisher, name='delete_publisher'),
+# Register your urls here.
 
-    path('games/', views.list_games, name='list_games'),
-    path('game/<int:id>', views.get_game, name='get_game'),
-    path('games_by/<int:id>', views.get_games_by_publisher, name='get_games_by_publisher'),
-    path('create_game/', views.create_game, name='create_game'),
-    path('update_game/<int:id>', views.update_game, name='update_game'),
-    path('delete_game/<int:id>', views.delete_game, name='delete_game'),
+urlpatterns = [    
+    path('publisher/', views.AlbumView.as_view(), name='publisher'),
+    path('publisher/<int:id>', views.AlbumViewId.as_view(), name='publisher-id'),
+    
+    path("game/", views.FaixaView.as_view(), name="game"),
+    path("game/<int:id>", views.FaixaViewId.as_view(), name="game-id"),
+    path("game/<int:publisher>", views.FaixaViewPublisher.as_view(), name="game-publisher"),
+    
 ]
--- GamesLibrary/Games/views.py
+++ GamesLibrary/Games/views.py
@@ -1,322 +1,293 @@
 import logging
 
 from django.shortcuts import get_object_or_404
+from .models import Game, Publisher
+from .serializers import GameSerializer, PublisherSerializer 
+from .exceptions import PublisherIDNotFoundException, GameIDNotFoundException
 
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework import status
 
 from drf_spectacular.utils import extend_schema
-
-from .models import Game, Publisher
-from .serializers import GameSerializer, PublisherSerializer 
-
-from .exceptions import PublisherIDNotFoundException, GameIDNotFoundException
-from .validation import validate_data
 
 # Create your views here.
 
 logger = logging.getLogger('Test: ')
 
-# <=-=> Publisher Urls <=-=>
+# -=-=- Publisher Urls -=-=-
 
+@extend_schema(tags=['Publisher'])
+class PublisherView(APIView):
+    def get(self, request):
+        try:
 
-@extend_schema(methods=['GET'], tags=['Publisher'])
-@api_view(['GET'])
-def list_publishers(request):
-    try:
+            publishers = Publisher.objects.all()
 
-        publishers = Publisher.objects.all()
-
-        if not publishers:
-            logger.debug('No publisher found.')
-            return Response('Error while fetching publishers.', status=status.HTTP_400_BAD_REQUEST)
+            if not publishers:
+                logger.debug('No publisher found.')
+                return Response('Error while fetching publishers.', status=status.HTTP_400_BAD_REQUEST)
+            
+            serializer = PublisherSerializer(publishers, many=True)
+            return Response(serializer.data)
         
-        serializer = PublisherSerializer(publishers, many=True)
-        return Response(serializer.data)
-    
-    except Exception as e:
+        except Exception as e:
 
-        logger.error(e)
-        return Response(
-            {
-                'status': 'error',
-                'message': 'Error while listing publishers.',
-                'error': str(e)
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-        )
-
-
-@extend_schema(methods=['GET'], tags=['Publisher'])
-@api_view(['GET'])
-def get_publisher(request, id):
-    try:
-        
-        publisher = Publisher.objects.get(id=id)
-        serializer = PublisherSerializer(publisher)
-        return Response(serializer.data)
-    
-    except Publisher.DoesNotExist:
-
-        logger.debug('Publisher with given ID not found.')
-        raise PublisherIDNotFoundException('Publisher with given ID not found.')
-
-    except Exception as e:
-
-        logger.error(e)
-        return Response(
-            {
-                'status': 'error',
-                'message': 'Error while listing publishers.',
-                'error': str(e)
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-        )
-
-
-@extend_schema(methods=['POST'], tags=['Publisher'])
-@api_view(['POST'])
-def create_publisher(request):
-    try:
-
-        serializer = PublisherSerializer(data=request.data)
-        
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
-        else:
-            logger.error('Publisher already exists.')
+            logger.error(e)
             return Response(
                 {
-                    'error': 'Publisher already exists.'
-                }, status=status.HTTP_400_BAD_REQUEST)
+                    'status': 'error',
+                    'message': 'Error while listing publishers.',
+                    'error': str(e)
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
     
-    except Exception as e:
+    def post(self, request):
+        try:
 
-        logger.error(e)
-        return Response(
-            {
-                'status': 'error',
-                'message': 'Error while creating publisher.',
-                'error': str(e)
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-        )
-    
-
-@extend_schema(methods=['PUT'], tags=['Publisher'])
-@api_view(['PUT'])
-def update_publisher(request, id):
-    try:
-
-        publisher = get_object_or_404(Publisher, id=id)
-
-        serializer = PublisherSerializer(publisher, data=request.data, partial=True)
+            serializer = PublisherSerializer(data=request.data)
+            
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            
+            else:
+                logger.error('Publisher already exists.')
+                return Response(
+                    {
+                        'error': 'Publisher already exists.'
+                    }, status=status.HTTP_400_BAD_REQUEST)
         
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        
-        logger.error(serializer.errors)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    except Exception as e:
-        logger.error(e)
-        return Response(
-            {
-                'status': 'error',
-                'message': 'Error while updating publisher.',
-                'error': str(e)
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-        )
-    
+        except Exception as e:
 
-@extend_schema(methods=['DELETE'], tags=['Publisher'])
-@api_view(['DELETE'])
-def delete_publisher(request, id):
-    try:
-
-        publisher = get_object_or_404(Publisher, id=id)
-        publisher.delete()
-        
-        return Response(
-            {'message': 'Publisher deleted successfully.'},
-            status=status.HTTP_204_NO_CONTENT
-        )
-    
-    except Exception as e:
-        logger.error(e)
-        return Response(
-            {
-                'status': 'error',
-                'message': 'Error while deleting publisher.',
-                'error': str(e)
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-        )
-
-
-# <=-=> Games Urls <=-=>
-
-
-@extend_schema(methods=['GET'], tags=['Publisher'])
-@api_view(['GET'])
-def list_games(request):
-    try:
-
-        games = Game.objects.all()
-        
-        if not games:
-            logger.debug('No publisher found.')
-            return Response('Error while fetching games.', status=status.HTTP_400_BAD_REQUEST)
-        
-        serializer = GameSerializer(games, many=True)
-        return Response(serializer.data)
-    
-    except Exception as e:
-
-        logger.error(e)
-        return Response(
-            {
-                'status': 'error',
-                'message': 'Error while listing games.',
-                'error': str(e)
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-        )
-
-
-@extend_schema(methods=['GET'], tags=['Game'])
-@api_view(['GET'])
-def get_game(request, id):
-    try:
-        
-        game = Game.objects.get(id=id)
-        serializer = GameSerializer(game)
-        return Response(serializer.data)
-    
-    except Game.DoesNotExist:
-
-        logger.debug('Game with given ID not found.')
-        raise GameIDNotFoundException('Game with given ID not found.')
-
-    except Exception as e:
-
-        logger.error(e)
-        return Response(
-            {
-                'status': 'error',
-                'message': 'Error while listing games.',
-                'error': str(e)
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-        )
-
-
-@extend_schema(methods=['GET'], tags=['Game'])
-@api_view(['GET'])
-def get_games_by_publisher(request, id):
-    try:
-        
-        games = Game.objects.filter(publisher=id)
-
-        if not games.exists():
-
-            logger.debug('No games found for the given publisher ID.')
-
+            logger.error(e)
             return Response(
-                {'error': 'No games found for the given publisher ID.'},
-                status=status.HTTP_404_NOT_FOUND
+                {
+                    'status': 'error',
+                    'message': 'Error while creating publisher.',
+                    'error': str(e)
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+
+@extend_schema(tags=['Publisher'])
+class PublisherViewId(APIView):
+    def get(self, request, id):
+        try:
+
+            publisher = Publisher.objects.get(id=id)
+            serializer = PublisherSerializer(publisher)
+            return Response(serializer.data)
+        
+        except Publisher.DoesNotExist:
+
+            logger.debug('Publisher with given ID not found.')
+            raise PublisherIDNotFoundException('Publisher with given ID not found.')
+        
+        except Exception as e:
+
+            logger.error(e)
+            return Response(
+                {
+                    'status': 'error',
+                    'message': 'Error while listing publishers.',
+                    'error': str(e)
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
         
-        serializer = GameSerializer(games, many=True)
-        return Response(serializer.data)
-    
-    except Exception as e:
+    def put(self, request, id):
+        try:
 
-        logger.error(e)
-        return Response(
-            {
-                'status': 'error',
-                'message': 'Error while listing games for publisher.',
-                'error': str(e)
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-        )
+            publisher = get_object_or_404(Publisher, id=id)
 
-
-@extend_schema(methods=['POST'], tags=['Game'])
-@api_view(['POST'])
-def create_game(request):
-    try:
-
-        serializer = GameSerializer (data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            serializer = PublisherSerializer(publisher, data=request.data, partial=True)
+            
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            
+            logger.error(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-        else:
-            logger.error('Game already exists.')
+        except Exception as e:
+            logger.error(e)
             return Response(
                 {
-                    'error': 'Game already exists.'
-                }, status=status.HTTP_400_BAD_REQUEST)
+                    'status': 'error',
+                    'message': 'Error while updating publisher.',
+                    'error': str(e)
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
     
-    except Exception as e:
+    def delete(self, request, id):
+        try:
 
-        logger.error(e)
-        return Response(
-            {
-                'status': 'error',
-                'message': 'Error while creating game.',
-                'error': str(e)
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-        )
-
-
-@extend_schema(methods=['PUT'], tags=['Game'])
-@api_view(['PUT'])
-def update_game(request, id):
-    try:
-
-        game = get_object_or_404(Game, id=id)
-
-        serializer = GameSerializer(game, data=request.data, partial=True)
+            publisher = get_object_or_404(Publisher, id=id)
+            publisher.delete()
+            
+            return Response(
+                {'message': 'Publisher deleted successfully.'},
+                status=status.HTTP_204_NO_CONTENT
+            )
         
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            logger.error(e)
+            return Response(
+                {
+                    'status': 'error',
+                    'message': 'Error while deleting publisher.',
+                    'error': str(e)
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+
+# -=-=- Games Urls -=-=-
+
+@extend_schema(tags=['Games'])
+class GamesView(APIView):
+    def get(self, request):
+        try:
+
+            games = Game.objects.all()
+            
+            if not games:
+                logger.debug('No publisher found.')
+                return Response('Error while fetching games.', status=status.HTTP_400_BAD_REQUEST)
+            
+            serializer = GameSerializer(games, many=True)
+            return Response(serializer.data)
         
-        logger.error(serializer.errors)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+
+            logger.error(e)
+            return Response(
+                {
+                    'status': 'error',
+                    'message': 'Error while listing games.',
+                    'error': str(e)
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
     
-    except Exception as e:
-        logger.error(e)
-        return Response(
-            {
-                'status': 'error',
-                'message': 'Error while updating publisher.',
-                'error': str(e)
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-        )
+    def post(self, request):
+        try:
 
+            serializer = GameSerializer(data=request.data)
 
-@extend_schema(methods=['DELETE'], tags=['Game'])
-@api_view(['DELETE'])
-def delete_game(request, id):
-    try:
-
-        game = get_object_or_404(Game, id=id)
-        game.delete()
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            
+            else:
+                logger.error('Game already exists.')
+                return Response(
+                    {
+                        'error': 'Game already exists.'
+                    }, status=status.HTTP_400_BAD_REQUEST)
         
-        return Response(
-            {'message': 'Game deleted successfully.'},
-            status=status.HTTP_204_NO_CONTENT
-        )
+        except Exception as e:
+
+            logger.error(e)
+            return Response(
+                {
+                    'status': 'error',
+                    'message': 'Error while creating game.',
+                    'error': str(e)
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+
+@extend_schema(tags=['Games'])
+class GamesViewId(APIView):
+    def get(self, request, id):
+        try:
+
+            game = Game.objects.get(id=id)
+            serializer = GameSerializer(game)
+            return Response(serializer.data)
+        
+        except Game.DoesNotExist:
+
+            logger.debug('Game with given ID not found.')
+            raise GameIDNotFoundException('Game with given ID not found.')
+        
+        except Exception as e:
+
+            logger.error(e)
+            return Response(
+                {
+                    'status': 'error',
+                    'message': 'Error while listing games.',
+                    'error': str(e)
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
     
-    except Exception as e:
-        logger.error(e)
-        return Response(
-            {
-                'status': 'error',
-                'message': 'Error while deleting game.',
-                'error': str(e)
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-        )
+    def put(self, request, id):
+        try:
+
+            game = get_object_or_404(Game, id=id)
+
+            serializer = GameSerializer(game, data=request.data, partial=True)
+            
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            
+            logger.error(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        except Exception as e:
+            logger.error(e)
+            return Response(
+                {
+                    'status': 'error',
+                    'message': 'Error while updating publisher.',
+                    'error': str(e)
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
     
+    def delete(self, request, id):
+        try:
+
+            game = get_object_or_404(Game, id=id)
+            game.delete()
+            
+            return Response(
+                {'message': 'Game deleted successfully.'},
+                status=status.HTTP_204_NO_CONTENT
+            )
+        
+        except Exception as e:
+            logger.error(e)
+            return Response(
+                {
+                    'status': 'error',
+                    'message': 'Error while deleting game.',
+                    'error': str(e)
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+
+@extend_schema(tags=['Games'])
+class GamesViewPublisher(APIView):
+    def get(self, request, publisher):
+        try:
+
+            games = Game.objects.filter(publisher=publisher)
+            
+            if not games:
+                logger.debug('No games found for the given publisher ID.')
+                return Response('Error while fetching games.', status=status.HTTP_400_BAD_REQUEST)
+            
+            serializer = GameSerializer(games, many=True)
+            return Response(serializer.data)
+        
+        except Exception as e:
+
+            logger.error(e)
+            return Response(
+                {
+                    'status': 'error',
+                    'message': 'Error while listing games.',
+                    'error': str(e)
+                }, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
